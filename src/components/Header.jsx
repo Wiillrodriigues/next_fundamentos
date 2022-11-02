08accc1e0efa875f25b893f78fac415452b9a630
Wiillrodriigues <86089098+Wiillrodriigues@users.noreply.github.com>
2022-11-02 17:31:12 -0300

    atualizando

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function cabecalho(){
+    return(
+        <header>
+            <h1>Fundamentos de Next.Js</h1>
+        </header>
+    )
+}
---
+++ src/pages/jsx.jsx
@@ -0,0 +1,11 @@
+export default function Jsx() {
+    const a = 31
+    const b = 12
+    return (
+        <div>
+            <h1>Jsx é um conceito Central</h1>
+            { a * b}
+            {/* {Math.random()} */}
+        </div>
+    )
+}
